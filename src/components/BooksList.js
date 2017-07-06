82baceaf89734f7abba0e3d699811b04f9f3c158
github username <github email address>
2017-07-06 19:21:11 -0400

    markers

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -4,7 +4,7 @@ import Book from './Book'
 export default function BooksList (props) {
 
   return (
-    <div>
+    <div id="myBookList" className="col-md-6">
       <h1>booklist</h1>
       <div className="row-eq-height">
         {props.books.map( book => <Book book={book} addUserBook={props.addUserBook} setBook={props.setBook} />  )}
---
+++ src/components/Map.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import Marker from './Marker'
+
+export default class Map extends Component {
+  static defaultProps = {
+    center: {lat: 40.705576 , lng: -74.013421},
+    zoom: 13
+  }
+
+
+  render() {
+    if (this.props.users.length === 0){
+      return <div> loading </div>
+    }
+
+    const Markers = this.props.users.map( user => < Marker key={user.id} lat={parseFloat(user.home_location.split(',')[0])} lng={parseFloat(user.home_location.split(',')[1])} />)
+    console.log(Markers)
+
+
+    return (
+
+      <div id="map">
+
+      <GoogleMapReact
+        defaultCenter={this.props.center}
+        defaultZoom={this.props.zoom}
+
+
+        bootstrapURLKeys={{
+          key: "AIzaSyBRAcZqVDsu2XEF9Sz-rQp_c-xWg7wMHRA",
+          language: 'en'
+          }}  >
+
+          {Markers}
+
+
+      </GoogleMapReact>
+    </div>
+    );
+  }
+}
